98a334942d1819c5abc756cad96c593a8fb3185e
Tomasz Lakomy <tomasz.lakomy@grandparade.co.uk>
2017-03-24 16:35:47 +0100

    Added awesomeness

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,14 @@ import fakeDelay from './fakeDelay';
 import path from 'path';
 
 let LoadableExample = Loadable({
-  loader: () => fakeDelay(400).then(() => import('./Example')),
-  LoadingComponent: Loading,
-  serverSideRequirePath: path.resolve(__dirname, './Example')
+  loader: () => fakeDelay(2000).then(() => import('./Example')),
+  LoadingComponent: Loading
 });
 
 export default function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>React Loadable Demo</h1>
       <LoadableExample/>
     </div>
   );
